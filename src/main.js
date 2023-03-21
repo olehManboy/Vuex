9016c412cf39aecf20da1e17e3e1029dc6356650
olehManboy <professiondev0706@gmail.com>
2023-03-21 05:14:07 -0700

    component registeration

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
-createApp(App).mount('#app')
+console.log(App, "first");
+App.component('HelloWorld', HelloWorld);
+console.log(App, "second")
+const app = createApp(App);
+
+app.mount('#app')
